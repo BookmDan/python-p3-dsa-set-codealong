f0d2d9391063d54a63aea2f2294d89a6ed38ebfe
BookmDan <danoh329@gmail.com>
2023-11-19 15:31:42 -0800

    pass

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,2 +1,20 @@
 class MySet:
-    def __init__(self, list =[])
+    def __init__(self, elements=None):
+        self.dictionary = {}
+        if elements:
+            for element in elements:
+                self.add(element)
+
+    def add(self, element):
+        self.dictionary[element] = True
+
+    def delete(self, element):
+        if self.has(element):
+            del self.dictionary[element]
+
+    def has(self, element):
+        return element in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
